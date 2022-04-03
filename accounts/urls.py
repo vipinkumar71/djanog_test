3ce7fe69314cc93dbf86fa2a2af1e12d4c6a4f55
Vipin Kumar <vipinthakur3881@gmail.com>
2022-04-03 17:04:36 +0530

    add login and register

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path("register", views.register, name='register')
+    path("register", views.register, name='register'),
+    path("login", views.login, name ='login'),
+    path("logout",views.logout, name="logout")
 ]
